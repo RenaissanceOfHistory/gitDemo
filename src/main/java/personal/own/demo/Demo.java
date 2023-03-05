04c92c3a9ca6c17ffc76b63b1bee7a9d1a6f3c2e
bj <bj@126.com>
2023-03-05 16:57:54 +0800

    git-demo test

--- src/main/java/personal/own/demo/Demo.java
+++ src/main/java/personal/own/demo/Demo.java
@@ -2,6 +2,8 @@ package personal.own.demo;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("这只一个demo程序。");
+        System.out.println("这是一个demo程序。");
+        System.out.println("这是一个demo程序。");
+        System.out.println("这是一个demo程序。");
     }
 }
